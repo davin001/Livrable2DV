04015478bd824770e2bf3cf487af5048935cdd2f
davin001 <104874449+davin001@users.noreply.github.com>
2022-05-12 22:12:06 -0400

    Update main.tf

--- aci/main.tf
+++ aci/main.tf
@@ -7,10 +7,10 @@ terraform {
 }
 terraform {
   backend "azurerm" {
-  resource_group_name  = "terraform"
+  resource_group_name   = "terraform"
   storage_account_name  = "tfstate6767"
-  container_name  = "tfstate"
-  key  = "terraform.tfstate"
+  container_name        = "tfstate"
+  key                   = "terraform.tfstate"
   }
 }
 provider "azurerm" {
